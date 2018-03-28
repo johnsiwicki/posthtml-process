01de2b778c0a5d3133b73d52968544d42d59bcc6
John Siwicki <johnsiwicki@gmail.com>
2018-03-28 14:10:42 -0400

    Updating index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var posthtml = require('posthtml');
 var customelements = require('posthtml-custom-elements');
-const collectInlineStyles  = require('posthtml-collect-inline-styles');
+var collectInlineStyles  = require('posthtml-collect-inline-styles');
 
 module.exports = function (context,cb) {
   
