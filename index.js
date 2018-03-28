4e96d47b0dee029c0df69b2d82e2cd8322ad12e9
John Siwicki <johnsiwicki@gmail.com>
2018-03-28 13:59:22 -0400

    Updating index.js

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-var express    = require('express');
-var Webtask    = require('webtask-tools');
-var bodyParser = require('body-parser');
 var posthtml = require('posthtml');
-var app = express();
+var customelements = require('posthtml-custom-elements');
+const collectInlineStyles  = require('posthtml-collect-inline-styles');
 
-app.use(bodyParser.json());
+module.exports = function (context,cb) {
+  
+   const bEmail = context.body.email;
+   const result = posthtml()
+    .use(customelements)
+    .use(collectInlineStyles)
+    .process(bEmail, { sync: true })
+    .html
+  
+    cb(null, {result});
+  
+};
 
-app.post('/', function (context, cb, req, res) {
-    const bEmail = context.body.email;
-    
-  const result = posthtml()
-  .use(require('posthtml-custom-elements')())
-  .process(bEmail, { sync: true })
-  .html
-
-console.log(result)
-    
-        
-});
-
-module.exports = Webtask.fromExpress(app);
