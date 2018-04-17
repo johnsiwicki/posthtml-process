b3888ec08b9704f08a0b3691ed69e9333e4074a3
John Siwicki <johnsiwicki@gmail.com>
2018-04-17 06:35:17 -0400

    Updating index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var posthtml = require('posthtml');
 var customelements = require('posthtml-custom-elements');
 var collectInlineStyles  = require('posthtml-collect-inline-styles');
 
-
 module.exports = function (context,cb) {
   
   const bEmail = context.body.email;
-  const result = posthtml()
-  
-    .use(customelements,collectInlineStyles)
+  const result = posthtml(
+      [
+        require('posthtml-custom-elements')(),
+        require('posthtml-collect-inline-styles')()
+      ])
     .process(bEmail, { sync: true })
     .html;
   
