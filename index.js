d3e4389910404f1a2624a805526430fa832f3a0e
John Siwicki <johnsiwicki@gmail.com>
2018-04-17 22:18:34 -0400

    Updating index.js

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const posthtml = require('posthtml');
 
-const postPlugins = [
-  require('posthtml-custom-elements')(),
-  require('posthtml-minifier')()
-];
- 
-
 module.exports = function (context,cb) {
   
   const bEmail = context.body.email;
   const result =  
-  posthtml([
-		require('posthtml-custom-elements')(),
-    require('posthtml-minifier')()
-	])
-    .process(bEmail, { sync: false })
-    .html;
+        posthtml([
+      		require('posthtml-custom-elements')(),
+          require('posthtml-minifier')()
+      	]).process(bEmail, { sync: false })
+          .html;
     
     cb(null, {result});
   
